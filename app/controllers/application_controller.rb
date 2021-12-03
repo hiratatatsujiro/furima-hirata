ad4978e216efaa3e07faa504a8691db2163b3b05
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-12-03 15:07:23 +0900

    devise can

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :devise_parameter_sanitizer,if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, 
+                                      keys: [:nickname, :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday])
+  end
 end
