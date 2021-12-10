5d2cc2087e2f735ce1d9a25597338e67613b3439
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-12-10 14:23:48 +0900

    login 修正

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,44 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_params, only: [:index, :create]
+
+
+  def index
+    @order_buyer = OrderBuyer.new
+    unless user_signed_in? && @order_buyer.user_id != current_user.id 
+      redirect_to root_path
+    end
+  end
+  
+  def create
+    @order_buyer = OrderBuyer.new(order_buyer_params)
+    if @order_buyer.valid?
+      pay_item
+      @order_buyer.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      @order_buyer = OrderBuyer.new(order_buyer_params)
+      render :index
+    end
+  end
+
+  private
+  def order_buyer_params
+    params.require(:order_buyer).permit(:postal_code, :delivery_area_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+        amount: @item.price,
+        card: @order_buyer.token,
+        currency: "jpy"
+      )
+  end
+
+  def find_params
+    @item = Item.find(params[:item_id]) 
+  end
+
+end
